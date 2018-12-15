8969b5f275c36d18e93c01f1ceb153b8e916c833
João Paulo Oliveira Cabral <jpocabral@gmail.com>
2018-12-14 23:22:06 -0200

    Update

--- GUI/csufg-confirmador.py
+++ GUI/csufg-confirmador.py
@@ -1,20 +1,6 @@
 from selenium import webdriver
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--window-size=1420,1080')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.binary_location = '/usr/bin/chromium-browser'
-
-service_log_path = "{}/chromedriver.log".format('/home/pi')
-
-#service_args = ['--verbose']
-
-browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver',
-                          chrome_options=chrome_options,
-                         #service_args=service_args,
-                          service_log_path=service_log_path)
+browser = webdriver.Chrome()
 browser.get('https://centrodeselecao.ufg.br/fiscalizacao/sistema/confirmacao/1_confirmacao_chamada.php')
 
 cpf = browser.find_element_by_id('cpf')
@@ -22,3 +8,6 @@ cpf.send_keys('70182005127')
 
 acessar = browser.find_element_by_class_name('btn')
 acessar.click()
+
+next = browser.find_element_by_class_name('btn-primary')
+next.click()
