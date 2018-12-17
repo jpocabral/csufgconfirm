372a229aa94518e7e00a7b565c907c6b072f574b
João Paulo Cabral <jpocabral@gmail.com>
2018-12-17 01:22:02 -0200

    Protótipo de modelo CLI unificado

--- GUI/csufg-confirmador.py
+++ GUI/csufg-confirmador.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from send_email import *
+import sys
+#-c or --cli
+#
+#var = ""
+#for param in sys.argv[1] :
+#    var += param
+#    var = var.split(',')
+#if len(var) > 2:
+#    print("Insira Apenas 2 argumentos!")
 
 browser = webdriver.Chrome()
 browser.get('https://centrodeselecao.ufg.br/fiscalizacao/sistema/confirmacao/1_confirmacao_chamada.php')
@@ -10,8 +20,10 @@ cpf.send_keys('70182005127')
 
 acessar = browser.find_element_by_class_name('btn')
 acessar.click()
+
+enviar_email('jpocabral@gmail.com','xxixacmhimbynbzz','jpocabral@gmail.com')
 try:
     confirmar_interesse = browser.find_element_by_class_name('btn-primary')
     confirmar_interesse.click()
 except NoSuchElementException:
-    print 'Não há concurso público disponível'
+        print 'Não há concurso público disponível'
--- GUI/send_email.py
+++ GUI/send_email.py
@@ -5,33 +5,31 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
-email_user = 'jpocabral@gmail.com'
-email_password = 'suasenha'
-email_send = 'jpocabral@gmail.com'
+def  enviar_email(usuario, senha, destinatario):
 
-subject = 'UFG - Concurso Confirmado'
+    subject = 'UFG - Concurso Confirmado'
 
-msg = MIMEMultipart()
-msg['From'] = email_user
-msg['To'] = email_send
-msg['Subject'] = subject
+    msg = MIMEMultipart()
+    msg['From'] = usuario
+    msg['To'] = destinatario
+    msg['Subject'] = subject
 
-body = 'Hi there, sending this email from Python!'
-msg.attach(MIMEText(body,'plain'))
-filename='teste.pdf'
-attachment  =open(filename,'rb')
+    body = 'Hi there, sending this email from Python!'
+    msg.attach(MIMEText(body,'plain'))
+    filename='teste.pdf'
+    attachment  =open(filename,'rb')
 
-part = MIMEBase('application','octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition',"attachment; filename= "+filename)
+    part = MIMEBase('application','octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',"attachment; filename= "+filename)
 
-msg.attach(part)
-text = msg.as_string()
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login(email_user,email_password)
+    msg.attach(part)
+    text = msg.as_string()
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(usuario ,senha)
 
 
-server.sendmail(email_user,email_send,text)
-server.quit()
+    server.sendmail(usuario,destinatario,text)
+    server.quit()
