ede76c753a7b001aeba47ac98ea63a5604f9f5ba
João Paulo Cabral <jpocabral@gmail.com>
2018-12-17 23:56:34 -0200

    --cli adicionado

--- GUI/csufg-confirmador.py
+++ GUI/csufg-confirmador.py
@@ -3,17 +3,29 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from send_email import *
 import sys
-#-c or --cli
-#
-#var = ""
-#for param in sys.argv[1] :
-#    var += param
-#    var = var.split(',')
-#if len(var) > 2:
-#    print("Insira Apenas 2 argumentos!")
+import argparse
 
-browser = webdriver.Chrome()
-browser.get('https://centrodeselecao.ufg.br/fiscalizacao/sistema/confirmacao/1_confirmacao_chamada.php')
+parser = argparse.ArgumentParser(description="CSUFGCONFIRM")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-c", "--cli", action="store_true")
+args = parser.parse_args()
+if args.cli:
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.binary_location = '/usr/bin/chromium-browser'
+
+    service_log_path = "{}/chromedriver.log".format('/home/pi')
+
+    browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver',
+                               chrome_options=chrome_options,
+                               service_log_path=service_log_path)
+    browser.get('https://centrodeselecao.ufg.br/fiscalizacao/sistema/confirmacao/1_confirmacao_chamada.php')
+else:
+    browser = webdriver.Chrome()
+    browser.get('https://centrodeselecao.ufg.br/fiscalizacao/sistema/confirmacao/1_confirmacao_chamada.php')
 
 cpf = browser.find_element_by_id('cpf')
 cpf.send_keys('70182005127')
@@ -21,9 +33,10 @@ cpf.send_keys('70182005127')
 acessar = browser.find_element_by_class_name('btn')
 acessar.click()
 
-enviar_email('jpocabral@gmail.com','xxixacmhimbynbzz','jpocabral@gmail.com')
+
 try:
     confirmar_interesse = browser.find_element_by_class_name('btn-primary')
     confirmar_interesse.click()
 except NoSuchElementException:
         print 'Não há concurso público disponível'
+        enviar_email('jpocabral@gmail.com', 'senha', 'jpocabral@gmail.com')
