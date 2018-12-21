41994f80eb878094bf1c38716c76c6ec937810e6
João Paulo Cabral <jpocabral@gmail.com>
2018-12-20 22:30:38 -0200

    1ª versão funcional com loop

--- GUI/csufg-confirmador.py
+++ GUI/csufg-confirmador.py
@@ -2,8 +2,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from send_email import *
-import sys
-import argparse
+import sys, argparse, time
 
 parser = argparse.ArgumentParser(description="CSUFGCONFIRM")
 
@@ -27,35 +26,65 @@ parser.add_argument('--senha',
                     type=str,
                     help = 'Senha do email informado')
 
+parser.add_argument('--chromedrivepath',
+                    action = 'store',
+                    dest = 'chromedrivepath',
+                    required = False,
+                    type=str,
+                    help = 'Local do binário do Chromedrive')
+
 args = parser.parse_args()
 
 email = args.email
 senha = args.senha
+chromedrivepath = args.chromedrivepath
 
 if args.cli:
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--window-size=1420,1080')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
+    while True:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1420,1080')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
 
-    browser = webdriver.Chrome(chrome_options=chrome_options)
+        browser = webdriver.Chrome(chrome_options=chrome_options)
 
-    browser.get('https://centrodeselecao.ufg.br/fiscalizacao/sistema/confirmacao/1_confirmacao_chamada.php')
+        browser.get('https://centrodeselecao.ufg.br/fiscalizacao/sistema/confirmacao/1_confirmacao_chamada.php')
+        # AUTENTICAÇÃO
+        cpf = browser.find_element_by_id('cpf')
+        cpf.send_keys('70182005127')
+        acessar = browser.find_element_by_class_name('btn')
+        acessar.click()
+
+        # VISUALIZA CONCURSOS DISPONÍVEIS
+        try:
+            confirmar_interesse = browser.find_element_by_class_name('btn-primary')
+            confirmar_interesse.click()
+        except NoSuchElementException:
+            print 'Não há concurso público disponível'
+            enviar_email(email, senha, assunto="UFG - Nenhum concurso no momento", texto="")
+        # TRATAMENTO DE CONCURSOS DISPONÍVEIS
+        # enviar_email(email, senha, texto="Glória a deux", assunto="UFG - Novo concurso disponível!")
+        time.sleep(30)
 else:
-    browser = webdriver.Chrome()
-    browser.get('https://centrodeselecao.ufg.br/fiscalizacao/sistema/confirmacao/1_confirmacao_chamada.php')
+    while True:
+        browser = webdriver.Chrome()
+        browser.get('https://centrodeselecao.ufg.br/fiscalizacao/sistema/confirmacao/1_confirmacao_chamada.php')
 
-cpf = browser.find_element_by_id('cpf')
-cpf.send_keys('70182005127')
+        #AUTENTICAÇÃO
+        cpf = browser.find_element_by_id('cpf')
+        cpf.send_keys('70182005127')
+        acessar = browser.find_element_by_class_name('btn')
+        acessar.click()
 
-acessar = browser.find_element_by_class_name('btn')
-acessar.click()
+        #VISUALIZA CONCURSOS DISPONÍVEIS
+        try:
+            confirmar_interesse = browser.find_element_by_class_name('btn-primary')
+            confirmar_interesse.click()
+        except NoSuchElementException:
+                print 'Não há concurso público disponível'
+                enviar_email(email, senha, texto="Não existem concursos no momento.", assunto="UFG - Nenhum concurso no momento")
 
-
-try:
-    confirmar_interesse = browser.find_element_by_class_name('btn-primary')
-    confirmar_interesse.click()
-except NoSuchElementException:
-        print 'Não há concurso público disponível'
-        enviar_email(email, senha)
+        #TRATAMENTO DE CONCURSOS DISPONÍVEIS
+        #enviar_email(email, senha, texto="Glória a deux", assunto="UFG - Novo concurso disponível!")
+        time.sleep(30)
--- GUI/send_email.py
+++ GUI/send_email.py
@@ -5,31 +5,28 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
-def  enviar_email(email, senha):
-
-    subject = 'UFG - Concurso Confirmado'
+def  enviar_email(email, senha, texto, assunto):
 
     msg = MIMEMultipart()
     msg['From'] = email
     msg['To'] = email
-    msg['Subject'] = subject
+    msg['Subject'] = assunto
 
-    body = 'Hi there, sending this email from Python!'
-    msg.attach(MIMEText(body,'plain'))
-    filename='teste.pdf'
-    attachment  =open(filename,'rb')
+    msg.attach(MIMEText(texto,'plain'))
+    #filename='teste.pdf'
+    #attachment  =open(filename,'rb')
 
-    part = MIMEBase('application','octet-stream')
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition',"attachment; filename= "+filename)
+    #part = MIMEBase('application','octet-stream')
+    #part.set_payload((attachment).read())
+    #encoders.encode_base64(part)
+    #part.add_header('Content-Disposition',"attachment; filename= "+filename)
 
-    msg.attach(part)
-    text = msg.as_string()
+    #msg.attach(part)
+    mensagem = msg.as_string()
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
     server.login(email ,senha)
 
 
-    server.sendmail(email,email,text)
+    server.sendmail(email,email,mensagem)
     server.quit()
