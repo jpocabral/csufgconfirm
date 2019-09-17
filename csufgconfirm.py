d481525d389da52c3db4484c5a67a00f0e1ea009
João Paulo Cabral <jpocabral@gmail.com>
2019-09-16 23:56:44 -0300

    Enxugamento das funcionalidades

--- csufgconfirm.py
+++ csufgconfirm.py
@@ -4,61 +4,38 @@ from selenium.common.exceptions import NoSuchElementException
 from send_email import enviar_email
 import time
 
-def iniciar(cpf, email, senha, cdpath, modo):
+def iniciar(cpf, email, senha, cdpath):
 
     url = 'https://centrodeselecao.ufg.br/fiscalizacao/sistema/confirmacao/1_confirmacao_chamada.php'
 
-    if modo is 'GUI' or modo is 'gui':
-        print ('MODO GUI')
-        while True:
-            browser = webdriver.Chrome(cdpath)
-            browser.get(url)
 
-            #AUTENTICAÇÃO
-            campocpf = browser.find_element_by_id('cpf')
-            campocpf.send_keys(cpf)
-            acessar = browser.find_element_by_class_name('btn')
-            acessar.click()
+    while True:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1420,1080')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
 
-            #VISUALIZA CONCURSOS DISPONÍVEIS
-            try:
-                confirmar_interesse = browser.find_element_by_class_name('btn-primary')
-                confirmar_interesse.click()
+        browser = webdriver.Chrome(cdpath,chrome_options=chrome_options)
 
-                # TRATAMENTO DE CONCURSOS DISPONÍVEIS
-                print ('Há um concurso público disponível')
-                enviar_email(email, senha, texto="Mais informações em " + url, assunto="UFG - Novo concurso disponível!")
-            except NoSuchElementException:
-                    print ('Não há concurso público disponível')
-            browser.quit()
-            time.sleep(30)
+        browser.get(url)
+        # AUTENTICAÇÃO
+        campocpf = browser.find_element_by_id('cpf')
+        campocpf.send_keys(cpf)
+        acessar = browser.find_element_by_class_name('btn')
+        acessar.click()
 
-    elif modo is 'CLI' or modo is 'cli':
-        while True:
-            chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--window-size=1420,1080')
-            chrome_options.add_argument('--headless')
-            chrome_options.add_argument('--disable-gpu')
+        # VISUALIZA CONCURSOS DISPONÍVEIS
+        try:
+            confirmar_interesse = browser.find_element_by_class_name('btn-primary')
+            confirmar_interesse.click()
 
-            browser = webdriver.Chrome(cdpath,chrome_options=chrome_options)
+            # TRATAMENTO DE CONCURSOS DISPONÍVEIS
+            print ('Há concursos públicos disponíveis')
+            enviar_email(email, senha, texto="Mais informações em "+url, assunto="UFG - Novo concurso disponível!")
+        except NoSuchElementException:
+            print ('Não há concurso público disponível')
+        browser.quit()
+        time.sleep(30)
 
-            browser.get(url)
-            # AUTENTICAÇÃO
-            campocpf = browser.find_element_by_id('cpf')
-            campocpf.send_keys(cpf)
-            acessar = browser.find_element_by_class_name('btn')
-            acessar.click()
-
-            # VISUALIZA CONCURSOS DISPONÍVEIS
-            try:
-                confirmar_interesse = browser.find_element_by_class_name('btn-primary')
-                confirmar_interesse.click()
-
-                # TRATAMENTO DE CONCURSOS DISPONÍVEIS
-                print ('Há concursos públicos disponíveis')
-                enviar_email(email, senha, texto="Mais informações em "+url, assunto="UFG - Novo concurso disponível!")
-            except NoSuchElementException:
-                print ('Não há concurso público disponível')
-            browser.quit()
-            time.sleep(30)
+iniciar("70182005127","notificadorbot@gmail.com","notifybot","C:\\PROGRA~1\Chromedriver\chromedriver.exe")
